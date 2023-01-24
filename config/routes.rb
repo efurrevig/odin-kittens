59f25d51fd411ed8d9d65818b55110fb2cc1f9e7
Eric Furrevig <efurrevig@gmail.com>
2023-01-24 18:44:15 -0500

    basic RESTful actions, views, and forms

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 Rails.application.routes.draw do
-  get 'kittens/index'
-  get 'kittens/show'
-  get 'kittens/new'
-  get 'kittens/create'
-  get 'kittens/edit'
-  get 'kittens/update'
-  get 'kittens/destroy'
+  # get 'kittens/index'
+  # get 'kittens/show'
+  # get 'kittens/new'
+  # get 'kittens/create'
+  # get 'kittens/edit'
+  # get 'kittens/update'
+  # get 'kittens/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root "kittens#index"
+
+  resources :kittens
+
 end
